60631888431ddb83e57db023e248760ac2770535
Aviad Segev <aviads@checkpoint.com>
2025-01-27 14:57:31 +0200

    Forgot to expose the added health check methods

--- Package.swift
+++ Package.swift
@@ -19,8 +19,8 @@ let package = Package(
 	targets: [
         .binaryTarget(
             name: "GRDWireGuardKit",
-            url:"https://github.com/chkp-aviads/GuardianWireGuard/releases/download/1.3.0/GRDWireGuardKit.xcframework.zip",
-            checksum: "19668925177ed642182cd108aecd9e69e40eebe550e030360528256657586fb5"
+            url:"https://github.com/chkp-aviads/GuardianWireGuard/releases/download/1.3.1/GRDWireGuardKit.xcframework.zip",
+            checksum: "f1d1b4c3ec7505a761c2b2fb12022d7e499771f94e51be50941989cb3405e4d6"
         )
 	]
 )
