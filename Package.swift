91dd656f01196a9fe715ed1165142c81662eb170
Aviad Segev <aviads@checkpoint.com>
2025-01-28 15:38:42 +0200

    Updated package manifest

--- Package.swift
+++ Package.swift
@@ -19,8 +19,8 @@ let package = Package(
 	targets: [
         .binaryTarget(
             name: "GRDWireGuardKit",
-            url:"https://github.com/chkp-aviads/GuardianWireGuard/releases/download/1.3.2/GRDWireGuardKit.xcframework.zip",
-            checksum: "c0c370e3d5adfa948299bcf87d43925cd609e43643bbc03b580eb6e2a707dfb5"
+            url:"https://github.com/chkp-aviads/GuardianWireGuard/releases/download/1.3.3/GRDWireGuardKit.xcframework.zip",
+            checksum: "38c013b4d035635973415a18026295ea57df7f0775b6dfdd650a3fc5d7ec0d0d"
         )
 	]
 )
