d7fb2828e16b8ec86d7191925931cc98da2e3a19
Aviad Segev <aviads@checkpoint.com>
2025-02-10 14:51:43 +0200

    Bumped WireGuard version (proper closing of proxied connections)

--- Package.swift
+++ Package.swift
@@ -19,8 +19,8 @@ let package = Package(
 	targets: [
         .binaryTarget(
             name: "GRDWireGuardKit",
-            url:"https://github.com/chkp-aviads/GuardianWireGuard/releases/download/1.3.6/GRDWireGuardKit.xcframework.zip",
-            checksum: "42d511f801f97a9b30811ec02d0f66082e01e621562e2a6009a4466fe1b1d90b"
+            url:"https://github.com/chkp-aviads/GuardianWireGuard/releases/download/1.3.7/GRDWireGuardKit.xcframework.zip",
+            checksum: "02bf47d40baca9a237640187dba144affb216252b2a4d4d61308459be5ca3843"
         )
 	]
 )
