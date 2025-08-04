d8c5a10053bb88e4688083414f04b37f8fb559d9
Aviad Segev <aviads@checkpoint.com>
2025-08-04 14:11:05 +0300

    Added wgResolveDNS and wgCancelResolveDNS to resolve DNS via WireGuard

--- Package.swift
+++ Package.swift
@@ -19,8 +19,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "GRDWireGuardKit",
-            url:"https://github.com/chkp-aviads/GuardianWireGuard/releases/download/2.0.2/GRDWireGuardKit.xcframework.zip",
-            checksum: "d6ca070551e820fba4d5dc73378ba91bbb75c4af228d973fb1cfa9621a2c2c00"
+            url:"https://github.com/chkp-aviads/GuardianWireGuard/releases/download/2.1.0/GRDWireGuardKit.xcframework.zip",
+            checksum: "e5fe401a52ceb13f3eb1dc106e9a2f7e5142ca5fdac5ffa85d082ad671ab8d7f"
         )
     ]
 )
